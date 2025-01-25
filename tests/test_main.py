8b30ff037ace5a6137790651a5e468f14c9eec64
AlexRyoma <32269660+Ljons@users.noreply.github.com>
2025-01-26 01:45:36 +0200

    first commit3

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,6 @@
-import pytest
 
-# @pytest.fixture()
-# def before_after():
-#     print("before")
-#     yield
-#     print("\nafter")
 def test_first():
     assert 1==1
 
-def test_first2(before_after):
+def test_first2(browser):
     assert 2==2
